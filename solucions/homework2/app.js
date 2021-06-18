b46839d1f1c2699b778867bbea93f0da62ae0a8d
MatPLay <matplay201020@gmail.com>
2021-06-18 13:10:07 +0300

    Homework2

---
+++ solucions/homework2/app.js
@@ -0,0 +1,16 @@
+var car = 'mercedes'
+var belief = undefined
+var randomNumber = 13
+let age = null
+let hasExperience = true
+let symbol = Symbol('zzz')
+const number = 1932472493248n
+const Matvei = {
+age=20,
+height = 186 ,
+name = 'Matvei',
+pastName = 'Egorov'
+}
+
+console.log(car)
+console.log(typeof car)
