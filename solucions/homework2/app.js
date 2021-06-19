fbbe25e9bf31fea7282e428017623dff992cdf28
MatPLay <matplay201020@gmail.com>
2021-06-19 14:00:41 +0300

    readyHomework2

---
+++ solucions/homework2/app.js
@@ -0,0 +1,26 @@
+var car = 'mercedes'
+var belief = undefined
+var randomNumber = 13
+let age = null
+let hasExperience = true
+let symbol = Symbol('zzz')
+const number = 1932472493248n
+const Matvei = {}
+
+
+console.log(car)
+console.log(typeof car)
+console.log(belief)
+console.log(typeof belief)
+console.log(randomNumber)
+console.log(typeof randomNumber)
+console.log(age)
+console.log(typeof age)
+console.log(hasExperience)
+console.log(typeof hasExperience)
+console.log(symbol)
+console.log(typeof symbol)
+console.log(number)
+console.log(typeof number)
+console.log(Matvei)
+console.log(typeof Matvei)
