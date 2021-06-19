b34b13f87c9081b1a166e90025289c5d4fe0ce5e
MatPLay <matplay201020@gmail.com>
2021-06-19 13:44:03 +0300

    homework2

--- solucions/homework2/app.js
+++ solucions/homework2/app.js
@@ -5,12 +5,21 @@ let age = null
 let hasExperience = true
 let symbol = Symbol('zzz')
 const number = 1932472493248n
-const Matvei = {
-age=20,
-height = 186 ,
-name = 'Matvei',
-pastName = 'Egorov'
-}
+const Matvei = {}
 
 console.log(car)
 console.log(typeof car)
+console.log(belief)
+console.log(typeof belief)
+console.log(randomNumber)
+console.log(typeof randomNumber)
+console.log(age)
+console.log(typeof age)
+console.log(hasExperience)
+console.log(typeof hasExperience)
+console.log(symbol)
+console.log(typeof symbol)
+console.log(number)
+console.log(typeof number)
+console.log(Matvei)
+console.log(typeof Matvei)
