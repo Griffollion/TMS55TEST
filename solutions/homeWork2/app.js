120397948988b5727789c0bbe6409fed9be5652d
YanaVarsak <85955470+YanaVarsak@users.noreply.github.com>
2021-06-20 04:17:10 +0300

    Yana. Finish homeWork-2

---
+++ solutions/homeWork2/app.js
@@ -0,0 +1,42 @@
+let userName = "Yana Varsak"
+console.log (userName)
+console.log(typeof userName)
+
+let userMood = true
+if (userMood){
+   console.log (':)')
+} else{
+    console.log(':(')
+} 
+console.log (typeof userMood)
+
+let userAge = 20
+console.log (userAge);
+console.log (typeof userAge)
+
+let getNan = 'Yana'/ 10
+console.log (getNan)
+console.log(typeof getNan)
+
+const bigInteger = 1238485853032032983199118439n
+console.log (bigInteger)
+console.log(typeof bigInteger)
+
+let userInfo = {
+    name: "Yana",
+    age: 20
+}
+console.log(userInfo)
+console.log(typeof userInfo)
+
+let id = Symbol("2")
+console.log(2)
+console.log (typeof id)
+
+var userPhone
+console.log(userPhone)
+console.log(typeof userPhone)
+
+let userCar = null
+console.log(userCar)
+console.log(typeof userCar)
