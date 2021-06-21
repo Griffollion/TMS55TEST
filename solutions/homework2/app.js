a18496349ea440ef9140919bbc42c0f3320f64a2
Andrey <hylaron@gmail.com>
2021-06-21 15:52:06 +0300

    Homework_2 with 8 variables and them calls

---
+++ solutions/homework2/app.js
@@ -0,0 +1,30 @@
+let PersoneName = 'Andrey'
+
+const PersoneAge = 12
+
+var PersoneHeight = undefined
+
+let PersoneSomething = null
+
+let isMoodGood = true
+
+let PersoneSymbol = Symbol('GhostRaven')    
+
+const PersoneNumber = 123124821331031932193
+
+let obj = {   
+  name: 'Afonya',
+  type: 'cat',
+  color: 'White body with black tail',
+  age: 3
+}
+
+console.log( typeof PersoneName)
+console.log( typeof PersoneAge)
+console.log( typeof PersoneHeight)
+console.log( typeof PersoneSomething)
+console.log( typeof isMoodGood)
+console.log( typeof PersoneSymbol)
+console.log( typeof PersoneNumber)
+console.log( typeof obj)
+
