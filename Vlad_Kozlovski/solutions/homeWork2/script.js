b39c3850a26913dc5e39b48bc5325097f3375959
kozlovski2704 <kozlovsi2704kozlovski@gmail.com>
2021-06-21 22:25:31 +0300

    new home work is Done

---
+++ Vlad_Kozlovski/solutions/homeWork2/script.js
@@ -0,0 +1,31 @@
+var booleanType = Boolean (false)
+console.log (typeof booleanType)
+console.log (booleanType)
+// =============================================================
+var stringType = String ("Hellow World");
+console.log (typeof stringType)
+console.log (stringType)
+// =============================================================
+var numberType = Number (11);
+console.log (typeof numberType)
+console.log (numberType)
+// =============================================================
+let nullType = null;
+console.log (typeof nullType)
+console.log (nullType)
+// =============================================================
+let undefinedType = undefined;
+console.log (typeof undefinedType)
+console.log (undefinedType)
+// =============================================================
+let  objectType = {type: "cat", name: "Bony", age: 13}; 
+console.log (typeof objectType)
+console.log (objectType)
+// =============================================================
+const symbolType = Symbol("WelLoveFE");
+console.log (typeof symbolType)
+console.log (symbolType)
+// =============================================================
+const bigIntType = BigInt(1n);
+console.log (typeof bigIntType)
+console.log (bigIntType)
