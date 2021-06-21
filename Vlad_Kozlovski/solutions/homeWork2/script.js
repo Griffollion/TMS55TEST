c8b712b4b05925c7d076f995d364d1d13e721c53
kozlovski2704 <kozlovsi2704kozlovski@gmail.com>
2021-06-21 22:40:46 +0300

    fixed

--- Vlad_Kozlovski/solutions/homeWork2/script.js
+++ Vlad_Kozlovski/solutions/homeWork2/script.js
@@ -1,12 +1,12 @@
-var booleanType = Boolean (false)
+var booleanType = false;
 console.log (typeof booleanType)
 console.log (booleanType)
 // =============================================================
-var stringType = String ("Hellow World");
+var stringType ="Hellow World";
 console.log (typeof stringType)
 console.log (stringType)
 // =============================================================
-var numberType = Number (11);
+var numberType = 11;
 console.log (typeof numberType)
 console.log (numberType)
 // =============================================================
@@ -22,7 +22,7 @@ let  objectType = {type: "cat", name: "Bony", age: 13};
 console.log (typeof objectType)
 console.log (objectType)
 // =============================================================
-const symbolType = Symbol("WelLoveFE");
+const symbolType = Symbol("WeLoveFE");
 console.log (typeof symbolType)
 console.log (symbolType)
 // =============================================================
