3a9686bda29aad84190c314c36b1561f05a050b1
artynskij <aartynskij@mail.ru>
2021-06-22 17:03:07 +0300

    Тёма победоносец домашка 2

---
+++ solution/homework-2/app.js
@@ -0,0 +1,35 @@
+let name= 'Artem'
+let surname = 'Artynskij'
+let nickName = 'JSFucker'
+let age = 23
+let race = undefined
+let haveMind = true
+var weight = 82
+let skillProgram = false
+
+
+
+console.log(typeof name )
+console.log ( name )
+
+console.log(typeof surname )
+console.log ( surname )
+
+console.log(typeof nickName )
+console.log ( nickName )
+
+console.log(typeof age )
+console.log( age )
+
+console.log(typeof race )
+console.log( race )
+
+
+console.log(typeof haveMind )
+console.log ( haveMind )
+
+console.log(typeof weight )
+console.log ( weight )
+
+console.log(typeof skillProgram )
+console.log ( skillProgram )
