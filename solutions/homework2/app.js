47f6cc1efdd67122d796f8a0359710dadb8d429b
AlexeyTrubko <ooo_ooo92@mail.ru>
2021-06-22 13:41:28 +0300

    origin

--- solutions/homework2/app.js
+++ solutions/homework2/app.js
@@ -1,7 +1,52 @@
-var userName ='Alexey'
-let age = 28
+
+var userName ='Alexey';
+alert(typeof 'userName');
+
+const myCity = 'Minsk';
+alert(typeof myCity);
+
+var age = 28;
+alert(typeof age);
+
+let myHeight = 1.8;
+alert(typeof myHeight);
+
+
 const isMale = true
 
+const bigInt = 1234567890123456789012345678901234567890n;
+alert(typeof bigInt)
 
 
-console
+let maritalStatus = 'married';
+alert(typeof materialStatus);
+
+
+var ageBaby = null;
+alert(typeof ageBaby);
+
+
+let iq = Symbol("iq");
+alert(typeof iq);
+console.log(typeof iq);
+
+console.log(typeof 'userName')
+console.log(userName)
+
+console.log(typeof 'age')
+console.log(age)
+
+console.log(typeof 'isMale')
+console.log(isMale)
+
+console.log(typeof 'myCity')
+console.log(myCity);
+
+console.log(typeof 'bigInt');
+console.log(bigInt)
+
+console.log(typeof 'materialStatus');
+console.log('materialStatus');
+
+console.log(typeof ageBaby);
+console.log(ageBaby);
