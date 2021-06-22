9d79ceafbefe07c4f6c0f14b3fc2c8d11664c5d8
oxidde <valeria.zen.v@gmail.com>
2021-06-22 10:45:46 +0300

    Зень: домашнее задание по JS2

---
+++ solutions/homework2/app.js
@@ -0,0 +1,29 @@
+const idStudent = Symbol("id")
+const studentName ='Valerya'
+let studentAge = 22
+let studentWorks =  true
+var studentWorkPosision = 'Software support specialist'
+let studentWorkExperience
+var studentWorkCommand = null
+var studentHobby = ['Text rp','Comics', 'Stretching', 'Photoshop']
+
+alert ('Закрой это уведомление и нажми F12')
+
+console.log('Какой тип данных у переменной idStudent?',typeof idStudent)
+console.log('Какой тип данных у переменной studentName?',typeof studentName)
+console.log('Какой тип данных у переменной studentAge?',typeof studentAge)
+console.log('Какой тип данных у переменной studentWorks?',typeof studentWorks)
+console.log('Какой тип данных у переменной studentWorkPosision?',typeof studentWorkPosision)
+console.log('Какой тип данных у переменной studentWorkExperience?',typeof studentWorkExperience)
+console.log('Какой тип данных у переменной studentWorkCommand?',typeof studentWorkCommand)
+console.log('Какой тип данных у переменной studentHobby?',typeof studentHobby)
+
+
+console.log('idStudent:', idStudent)
+console.log('studentName:',studentName)
+console.log('studentAge:',studentAge)
+console.log('studentWorks:', studentWorks)
+console.log('studentWorkPosision:', studentWorkPosision)
+console.log('studentWorkExperience:', studentWorkExperience)
+console.log('studentWorkCommand:', studentWorkCommand)
+console.log('studentHobby:', studentHobby)
