e983a96835509f0f3de1c6a8fade93262d119614
AlexeyTrubko <ooo_ooo92@mail.ru>
2021-06-24 15:37:41 +0300

    Trubko_Alexey-homework3

---
+++ solutions/homework3/app.js
@@ -0,0 +1,75 @@
+//ZADANIE 1
+
+const a = '1'
+const b = '2'
+const c = " белых медведей"
+
+const result = a + b + c
+console.log(result)
+
+//ZADANIE 2
+
+const A = 10
+const B = 2
+const C = 5
+
+const resultSum = A + B + C
+console.log(resultSum)
+
+//ZADANIE 3
+let num = 10;
+num = ++num;
+num = ++num;
+num = --num;
+alert(num);
+console.log(num)
+
+//ZADANIE 4
+ const a1 = 10
+ const a2 = 5
+ const a3 = 17
+ const a4 = 25 
+ const a5 = 500
+
+ console.log(Math.max(a1, a2, a3, a4, a5))
+
+ //ZADANIE 5
+ const a6 = 5 % 3
+ const a7 = 3 % 5
+ const a8 = 5 + '3'
+ const a9 =  '5' - 3 
+ const a10 = 75 + 'кг'
+
+ console.log(a6, a7, a8, a9, a10)
+
+//ZADANIE6
+
+    const b1 = 100
+    const b2 = 500
+    console.log(Math.min(b1, b2))
+    alert(Math.min(b1, b2))
+
+//ZADANIE9
+
+    let n = 0
+    let m = 1000
+    let sum = 0;
+    
+    for (let i = 0; i < 1000; i++) 
+    {
+        if (i % 4 == 0){ 
+            sum4.push(sum4[i]);
+            sum += i;
+        {if(i % 3 == 0){
+            sum3.push(sum4[i]);
+            sum += i;
+        }
+    }
+    sum3 << endl << sum4 << endl;
+    
+
+    const sum4 =[]
+    const sum3 =[]
+
+console.log('Sum4',sum4)
+console.log('Sum3',sum3)
