82f1cb1b4f4672179cb29bf9e580a44040af4c69
oxidde <valeria.zen.v@gmail.com>
2021-06-24 20:29:09 +0300

    Valerya Zen, JS: homework #3 fixed

--- solutions/homework3/app.js
+++ solutions/homework3/app.js
@@ -4,7 +4,8 @@ let a1 = 1
 let b1 = 2
 let c1 = " белых медведей"
 
-let resualt1 = a1 + b1 + c1
+let resualt1 = b1 + c1
+resualt1 = a1 + resualt1
 
 console.log ('Ответ на задание №1: ', resualt1)
 
