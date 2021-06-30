d4f0d595a2fa1780a532865ee59b75343e79e150
Dmitry <kosenko-dm@mail.ru>
2021-06-30 23:12:54 +0300

    done task 33

---
+++ solutions/homework-5/app.js
@@ -0,0 +1,27 @@
+'use strict'
+// задание 33
+
+console.log ('Решение задания 33:')
+
+let fruits = ['apple','banana','apple','lemon','banana','cherry','orange','orange','cherry','apple','peach']
+let makeDataFruits = () => {
+    let dataFruits = {}
+    let copyFruits = fruits
+   for (let i = 0; i < copyFruits.length; i++){
+    dataFruits[copyFruits[i]] = 1
+    for (let j = i+1; j < copyFruits.length; j++){
+    if (copyFruits[i] === copyFruits[j]){
+        ++dataFruits[copyFruits[i]]
+        copyFruits.splice(j,1)
+    } else {
+       continue
+    }
+}
+}
+console.log(dataFruits)
+}
+
+makeDataFruits()
+
+console.log ('Решение задания 34:')
+
