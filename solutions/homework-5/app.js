ad7dc9fd70b8d55737c0526d0ec213433bd7b169
Dmitry <kosenko-dm@mail.ru>
2021-06-30 23:41:27 +0300

    fixed copying an array

--- solutions/homework-5/app.js
+++ solutions/homework-5/app.js
@@ -6,7 +6,7 @@ console.log ('Решение задания 33:')
 let fruits = ['apple','banana','apple','lemon','banana','cherry','orange','orange','cherry','apple','peach']
 let makeDataFruits = () => {
     let dataFruits = {}
-    let copyFruits = fruits
+    let copyFruits = fruits.slice()
    for (let i = 0; i < copyFruits.length; i++){
     dataFruits[copyFruits[i]] = 1
     for (let j = i+1; j < copyFruits.length; j++){
