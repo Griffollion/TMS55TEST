18b255e1e9e8af634af51785a954730bdfa3008c
Your Name <tolkun.kolya@mail.ru>
2021-07-01 03:21:55 +0300

    Kolay_Tolkun. Complete task 19,20

---
+++ solutions/homework4/app4.js
@@ -0,0 +1,126 @@
+'use stcrict'
+
+
+
+//Задание 18
+
+// let someString = 'какая-то непонятная строка нужна для задания' 
+
+// function headingLetter (text) {
+//     let newText = text
+//     newText = text.toLowerCase()
+//     newText = newText.replace(newText.charAt(0), newText.charAt(0).toUpperCase())
+//     for (i = 0; i < newText.lenght; i++) {
+//         if (newText.charAt(i) === '')
+//         newText.replace(newText.charAt(i+1), newText.charAt(i+1).toUpperCase())
+//     }
+//     return newText
+// }
+
+
+// console.log(headingLetter(someString))
+
+
+
+//Задание 19
+
+// let sum = 0
+
+// function getSum (number) {
+//     for (let i = 0; i <= number; i++) {
+//         sum += i
+//     }
+//     return sum
+// }
+
+// console.log(getSum(100))
+
+
+
+//Задание 20
+
+// var numbers = [10, 25, 100, 'gaj', 'jgas']
+// let sum = 0
+
+// function getArraySum () {
+//     for (let i = 0; i < numbers.length; i++) {
+        
+//         if (typeof numbers[i] === "number") {
+//             sum += numbers[i]
+//         }
+//         console.log(sum)
+//     }
+//     return
+// }
+
+// console.log(getArraySum())
+
+
+
+//Задание 21
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
