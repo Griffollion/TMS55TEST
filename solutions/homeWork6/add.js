6460acb4435184c98005d06640fe47b0eb5f81fe
AlexKarseka <alexkarsoko@gmail.com>
2021-07-04 21:39:21 +0300

    Alex_Karseka-homeWork6

---
+++ solutions/homeWork6/add.js
@@ -0,0 +1,34 @@
+'use strict'
+
+//Задание 35
+//const numbers = [1,2,3]
+//numbers.reverse()
+//
+//console.log(numbers)
+
+// Задание 36
+// const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+// function calcSumNumbers (a, ...rest){
+//    return a + rest.reduce((a, i) => a + i, 0)
+// }
+
+// console.log(calcSumNumbers(...numbers))
+
+//Задание 37
+// const user = {
+//     name: 'Alex',
+//     age: 28,
+//     country: 'Belarus'
+//   };
+//   function convertObject(obj) {
+//     let user2 = {}
+//     let propertie = Object.keys(obj)
+//     propertie.forEach(function(value) {
+//       let key = obj[value]
+//       user2[key] = value 
+//     })                   
+//     return user2
+//   };
+  
+//   console.log(convertObject(user))
