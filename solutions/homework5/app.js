d6f02970db14adf7d8f8e515f8bd2b7d6d90d4c1
Your Name <tolkun.kolya@mail.ru>
2021-07-05 21:50:22 +0300

    Kolay_Tolkun. Finish tasks homeWork 5

---
+++ solutions/homework5/app.js
@@ -0,0 +1,65 @@
+'use struct'
+
+
+
+// Задание 33
+
+// const fruits = ['apple','apple','banana','apple','lemon','banana']
+
+// const numberOfFruits = fruits.reduce((number, fruit) => {
+//     number[fruit] = (number[fruit] || 0) + 1
+//     return number
+// }, {})
+
+// console.log(numberOfFruits)
+
+
+
+// Задание 34
+
+// const someObject = { a: 'b', c: 'd' }
+
+// function convertObject(obj) {
+//     const res = {}
+//     Object.keys(obj).forEach(function (value) {
+//       var key = obj[value]
+//       res[key] = value
+//     })
+//     return res
+// }
+
+// console.log(convertObject(someObject))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
