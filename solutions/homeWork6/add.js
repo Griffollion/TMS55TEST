7ec5faf806f0daba10a7d2e6089e28791c179e96
AlexKarseka <alexkarsoko@gmail.com>
2021-07-05 21:15:46 +0300

    Alex_Karseka-homeWork6-part2

--- solutions/homeWork6/add.js
+++ solutions/homeWork6/add.js
@@ -32,3 +32,69 @@
 //   };
   
 //   console.log(convertObject(user))
+
+//Задание 38
+// const arr = [
+//     { x: 1, z: 2, y: 3 },
+//     { x: 10, z: 21, y: 4 },
+//     { x: 2, z: 3, y: 4 },
+//     { a: 1, b: 2 },
+// ]
+
+// let x = arr.reduce((sum, current) => sum + (current.x || 0), 0)
+// let z = arr.reduce((sum, current) => sum + (current.z || 0), 0)
+// let y = arr.reduce((sum, current) => sum + (current.y || 0), 0)
+// let a = arr.reduce((sum, current) => sum + (current.a || 0), 0)
+// let b = arr.reduce((sum, current) => sum + (current.b || 0), 0)
+// let result = {x, z, y, a, b}
+
+// console.log(result)
+
+//Задание 39
+// const users = [
+//     {name: 'Petyn', gender: 'male'},
+//     {name: 'Olga', gender: 'female'},
+//     {name: 'Anna', gender: 'female'},
+//     {name: 'Igar', gender: 'male'},
+//     {name: 'Vasyn', gender: 'male'},
+// ]
+
+// let female = users.reduce((sum, value) => {
+//     if (value.gender == 'female'){
+//        sum.push(value)
+//     }
+//     return sum
+// },[])
+// let male = users.reduce((sum, value) => {
+//     if (value.gender == 'male'){
+//        sum.push(value)
+//     }
+//     return sum
+// },[])
+
+// console.log(female)
+// console.log(male)
+
+//Задание 40
+/* У меня небольшой вопрос, я создавал переменную для отфильтровки ненужных значений, мой код выглядел так
+const elements = [1, 3, false, ' ', 0, null, undefined, 'Alex'] 
+const unwantedEle = [false, ' ', 0, null, undefined]
+const stripping = elements.filter((value) => {
+   return value != unwantedEle
+})
+console.log (stripping)
+Но оно не сработало, где тут ошибка? Если можно подскажи) */
+
+
+// const elements = [1, 3, false, ' ', 0, null, undefined, 'Alex'] 
+// const stripping = elements.filter((value) => {
+//     return value != false && value != ' ' && value != 0 && value != null && value != undefined
+// })
+
+// console.log (stripping)
+
+//Задание 41
+// const numbers = [1,2,3,4,5]
+// while (numbers.length) {
+//     console.log(numbers.splice(0, 2))
+// }
