b6e67d27050e3464189462533dff7c72d855407e
artynskij <aartynskij@mail.ru>
2021-07-06 14:13:26 +0300

    Artyn homework-4

---
+++ solution/homework-4/app.js
@@ -0,0 +1,75 @@
+// 26.
+
+// let str = 'Flash Is mOrNing'
+// function changeWord() {
+//     var newStr = ""
+
+// for (let i = 0; i<str.length ; i++) {
+//     if (str[i]===str[i].toLowerCase()){
+//         newStr += str[i].toUpperCase()
+//     } else { newStr+=str[i].toLowerCase()}
+// }
+// return newStr
+// }
+// console.log(changeWord());
+
+// 27;
+// let number = "772276";
+// function pasteSymbol() {
+//   let lastNumber = "";
+//   for (let i = 0; i < number.length; i++) {
+//     if (number[i] % 2 === 1 && number[i + 1] % 2 === 1) {
+//       lastNumber += number[i] + ":";
+//     } else {
+//       lastNumber += number[i];
+//     }
+//   }
+//   return lastNumber;
+// }
+
+// console.log(pasteSymbol());
+
+// 28------непонятка
+// 29;------Калхоз стайл
+// let todayYear = '2021';
+
+// function calcYear(todayYear='2021') {
+
+//   let sumYear =Number('')
+//   for (let i = 0; i < todayYear.length; i++) {
+//     sumYear =sumYear + (todayYear[i]*2/2);
+//   }
+//   return sumYear;
+// }
+
+// console.log(calcYear('2017'));
+// 30.
+// let word = 'Hi my MASTER Younis(youda+denis)'
+// function transformKebabCase () {
+//   let changeWord = ''
+//   for (let i = 0; i<word.length; i++ ) {
+//     if (word[i] == ' ') {
+//       changeWord += '-'
+//     } else {changeWord+= word[i].toLowerCase()}
+//   }
+//   return changeWord
+// }
+
+// console.log(transformKebabCase());
+// 31; ??????????????????????????????????????????????????
+// let word = "United State"
+// function transformAbbreviation() {
+//   // return word.slice(0,1).toUpperCase()
+//   let changeWord = "";
+//   for (let i = 0; 0 < word.length; i++) 
+//   // changeWord += word[0].toUpperCase;
+//   if (word[i] == " ") {
+//     changeWord += word[i+1].toUpperCase;
+//   }
+//   return changeWord;
+// }
+// transformAbbreviation()
+// console.log(transformAbbreviation());
+
+
+32.
