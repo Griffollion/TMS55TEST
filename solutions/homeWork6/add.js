ca8a400c996cbc9fae151a0a288ed0479e36a9df
AlexKarseka <alexkarsoko@gmail.com>
2021-07-06 10:15:10 +0300

    Alex_Karseka-homeWork6-part3

--- solutions/homeWork6/add.js
+++ solutions/homeWork6/add.js
@@ -8,12 +8,8 @@
 
 // Задание 36
 // const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
-// function calcSumNumbers (a, ...rest){
-//    return a + rest.reduce((a, i) => a + i, 0)
-// }
-
-// console.log(calcSumNumbers(...numbers))
+// const calcSumNumbers = numbers.reduce((a, i) => a + i, 0)
+// console.log(calcSumNumbers)
 
 //Задание 37
 // const user = {
@@ -35,20 +31,29 @@
 
 //Задание 38
 // const arr = [
-//     { x: 1, z: 2, y: 3 },
-//     { x: 10, z: 21, y: 4 },
-//     { x: 2, z: 3, y: 4 },
-//     { a: 1, b: 2 },
+// { x: 1, z: 2, y: 3 },
+// { x: 10, z: 21, y: 4 },
+// { x: 2, z: 3, y: 4 },
+// { a: 1, b: 2 },
 // ]
 
-// let x = arr.reduce((sum, current) => sum + (current.x || 0), 0)
-// let z = arr.reduce((sum, current) => sum + (current.z || 0), 0)
-// let y = arr.reduce((sum, current) => sum + (current.y || 0), 0)
-// let a = arr.reduce((sum, current) => sum + (current.a || 0), 0)
-// let b = arr.reduce((sum, current) => sum + (current.b || 0), 0)
-// let result = {x, z, y, a, b}
+// const summationObjectsProperties = (arr) => {
+// const result = arr.reduce((accumulator, item) => {
+// const keys = Object.keys(item)
+// keys.forEach(el => {
+// if(accumulator[el]) {
+// accumulator[el] += item[el]
+// } else {
+// accumulator[el] = item[el]
+// }
+// })
+// return accumulator
+// }, {})
 
-// console.log(result)
+// return result
+// }
+
+// console.log(summationObjectsProperties(arr))
 
 //Задание 39
 // const users = [
@@ -76,25 +81,16 @@
 // console.log(male)
 
 //Задание 40
-/* У меня небольшой вопрос, я создавал переменную для отфильтровки ненужных значений, мой код выглядел так
-const elements = [1, 3, false, ' ', 0, null, undefined, 'Alex'] 
-const unwantedEle = [false, ' ', 0, null, undefined]
-const stripping = elements.filter((value) => {
-   return value != unwantedEle
-})
-console.log (stripping)
-Но оно не сработало, где тут ошибка? Если можно подскажи) */
-
-
-// const elements = [1, 3, false, ' ', 0, null, undefined, 'Alex'] 
-// const stripping = elements.filter((value) => {
-//     return value != false && value != ' ' && value != 0 && value != null && value != undefined
-// })
+// const elements = [1, 3, false, '', 0, null, undefined, 'Alex'] 
+// const stripping = elements.filter(el => el)
 
 // console.log (stripping)
 
 //Задание 41
 // const numbers = [1,2,3,4,5]
+// const separator = []
 // while (numbers.length) {
-//     console.log(numbers.splice(0, 2))
+//    separator.push(numbers.splice(0, 2))
 // }
+
+// console.log(separator)
