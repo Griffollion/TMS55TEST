d22a2859abe875a93346028f3a261a19dcbe257d
Dmitry <kosenko-dm@mail.ru>
2021-07-07 17:14:34 +0300

    done tasts 42,43

---
+++ solutions/homework-7/app.js
@@ -0,0 +1,39 @@
+'use strict'
+
+задание 42
+
+console.log('Решение задания 42:')
+
+function Calculator() {
+        let oneValue
+        let twoValue
+    this.read = function(){
+        oneValue = +prompt('Введите первое значение')
+        twoValue = +prompt('Введите второе значение')
+        return [oneValue, twoValue]
+        
+    }
+    this.sum = function(){
+        return oneValue + twoValue
+    }
+    this.mul = function(){
+        return oneValue*twoValue
+    }
+
+}
+
+let calculator = new Calculator()
+    calculator.read()
+    alert ('Sum='+ calculator.sum() )
+    alert ('Mul='+ calculator.mul() )
+
+// задание 43
+
+console.log('Решение задания 43:')
+
+let sumTo = (n) => (n == 1) ? n : sumTo(n-1) + n
+
+let resultSum = sumTo(100)
+
+console.log(resultSum)
+
