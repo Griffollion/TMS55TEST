1b83030b7d9577e81c8614d3c40400751d66b099
Karina_Romashko_12 <romashkok12@gmail.com>
2021-07-09 12:27:00 +0300

    Karina_Romashko-homeWork_6--js

---
+++ solutions/homeWork6/app.js
@@ -0,0 +1,9 @@
+/////////////HW6//////////////
+//Задание 35
+// let arr = [1, 2, 3]
+// let newArr = arr.reverse()
+// console.log(newArr)
+//Задание 36
+// let arr = [0, 2, 3, 2, 5, 10, 4, 8, 9, 0, 6, 12, 3, 0, 9, 7, 5, 2, 9, 0]
+// console.log ( arr.reduce((sum, item) => sum + item, 0))
+//Задание 
