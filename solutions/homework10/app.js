6e7953be99b381dbfc51fa18f2a3f7a2829974aa
rekunolya <rekun.olya@gmail.com>
2021-07-19 01:03:41 +0300

    Рекун Ольга домашка 10 со звездочкой. При повторном нажатии на кнопку submit  не меняются, а добавляются классы, если не перезагружать страничку

--- solutions/homework10/app.js
+++ solutions/homework10/app.js
@@ -1,14 +1,40 @@
 'use strict'
 const form = document.forms['user_form']
-const userName = form.user_name;
-const userPassword = form.password;
+const userName = form.user_name
+const userPassword = form.password
+const submitBtn = form.action
 
-const textName = document.querySelector('[data-realtime-name]');
+const textName = document.querySelector('[data-realtime-name]')
 userName.addEventListener('input', () => {
-textName.innerText = userName.value
+  textName.innerText = userName.value
 })
 
-const textPassword = document.querySelector('[data-realtime-password]');
+const textPassword = document.querySelector('[data-realtime-password]')
 userPassword.addEventListener('input', () => {
-textPassword.innerText = userPassword.value
+  textPassword.innerText = userPassword.value
+})
+
+function validateNameAndPassword() {
+  if (userName.value != '' && userPassword.value.length >= 5) {
+    userName.classList.add('valid')
+    userPassword.classList.add('valid')
+    alert('Форма заполнена правильно')
+  } else if (userName.value != '' && userPassword.value.length < 5){
+    userPassword.classList.add('invalid')
+    userName.classList.add('valid')
+    alert('Пароль должен быть более 5 символов')
+  } else if (userName.value = '' && userPassword.value.length >= 5) {
+      userName.classList.add('invalid')
+      userPassword.classList.add('valid')
+      alert('Введите имя')
+  } else {
+      userName.classList.add('invalid');
+      userPassword.classList.add('invalid');
+      alert('Введите имя и пароль не менее 5 символов')
+  }
+}
+
+form.addEventListener('submit', (el) => {
+  el.preventDefault()
+  validateNameAndPassword(form)
 })
