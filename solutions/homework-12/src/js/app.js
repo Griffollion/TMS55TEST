71be8f227e9323f4ca1cde83b95b9f70a47149ca
Dmitry <kosenko-dm@mail.ru>
2021-07-24 23:07:38 +0300

    my done homework12

--- solutions/homework-12/src/js/app.js
+++ solutions/homework-12/src/js/app.js
@@ -1,33 +1,41 @@
-const container = document.querySelector('#app')
+const container = document.querySelector("#app");
 
 function makeCardsList(arr) {
-    let section = document.createElement('section')
-    section.className = 'section-cards'
-    arr.forEach((element) => {
+  let section = document.createElement("section");
+  section.className = "section-cards wrapper";
+  arr.forEach((element) => {
     let cardDiv = document.createElement("div");
     cardDiv.className = "card-film";
     cardDiv.innerHTML = `
-    <div class="card-img">
-        <img src="${element.img}"></img>
-    </div>
-    <div class="card-title">
-        ${element.title}
-    </div>
-    <div class="card-date">
-        ${element.date}
-    </div>
-    <div class="card-price">
-        ${element.price}
-    </div>
+    <a href="${element.URL}">
+        <div class="card-img">
+            <img src="${element.img}"></img>
+        </div>
+        <div class="card-title">
+            ${element.title}
+        </div>
+        <div class="card-date">
+            <div class="mb-5">
+                ${element.date}
+            </div>
+            <div>
+                ${element.price}
+            </div>
+        </div>
+    </a>
         `;
-    section.append(cardDiv)
+    section.append(cardDiv);
   });
-  return section
+  return section;
 }
 
-async function getDataFromServer(){
-    const response = await fetch ('/solutions/homework-12/src/data/movies.json')
-    const data = await response.json()
-    
+async function getDataFromServer() {
+  const response = await fetch("/solutions/homework-12/src/data/movies.json");
+  const data = await response.json();
+  container.append(makeCardsList(data));
 }
+const h1 = document.createElement("h1");
+h1.innerHTML = `Фильмы в кинотеатрах Минска`;
+container.append(h1);
 
+getDataFromServer();
